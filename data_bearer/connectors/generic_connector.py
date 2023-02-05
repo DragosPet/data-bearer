8f09439c00f8639489f64f27b5c46405e28b0b58
DragosPet <iuliandragospet@gmail.com>
2023-02-05 20:51:24 +0200

    Add base connectivity class

---
+++ data_bearer/connectors/generic_connector.py
@@ -0,0 +1,50 @@
+"""
+Generic connector abstract class, for future inheritance of specific connectors.
+"""
+from abc import ABC
+import logging
+
+
+class Connector(ABC):
+    """Abstract class for generic connector component."""
+
+    __package_name__ = None
+
+    def __init__(self) -> None:
+        pass
+
+    def set_logger(self, log_level="WARN", use_file="n", path=None):
+        """Define logging properties for classes"""
+        if log_level == "INFO":
+            logging_level = logging.INFO
+        elif log_level == "WARN":
+            logging_level = logging.WARN
+        else:
+            logging_level = logging.ERROR
+        if use_file == "y":
+            if path:
+                logging.basicConfig(
+                    format="%(asctime)s %(levelname)s %(message)s",
+                    datefmt="%m/%d/%Y %I:%M:%S",
+                    filename=f"{path}/{self.__package_name__}.log",
+                    filemode="a",
+                    level=logging_level,
+                )
+            else:
+                logging.basicConfig(
+                    format="%(asctime)s %(levelname)s %(message)s",
+                    datefmt="%m/%d/%Y %I:%M:%S",
+                    level=logging_level,
+                )
+        else:
+            logging.basicConfig(
+                format="%(asctime)s %(levelname)s %(message)s",
+                datefmt="%m/%d/%Y %I:%M:%S",
+                level=logging_level,
+            )
+
+    def __str__(self) -> str:
+        return str(self.__package_name__)
+
+    def __repr__(self) -> str:
+        return str(self.__package_name__)
---
+++ tests/test_version.py
@@ -0,0 +1,4 @@
+from data_bearer import __version__
+
+def test_version() :
+    assert(__version__ == '0.1.0')
