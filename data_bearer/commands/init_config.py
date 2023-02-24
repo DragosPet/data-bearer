efe480f85934182b02283a89ddc82acebdcd6d56
DragosPet <42781932+DragosPet@users.noreply.github.com>
2023-02-24 19:19:29 +0200

    Dev (#9)
    * Add sqlite functionality
    
    * Add unit test for sqlite connector
    
    * Change init config for interactive mode

--- data_bearer/commands/init_config.py
+++ data_bearer/commands/init_config.py
@@ -2,60 +2,23 @@
 Generation of a run_config.yaml that will be used in the execution)."""
 
 import os
-import configargparse
+from getpass import getpass
 from data_bearer.utils.run_utils import persist_local_config, format_header
 
-parser = configargparse.ArgParser()
 
-parser.add(
-    "-w",
-    "--workdir",
-    required=True,
-    help="Workdir path for storing and reading the state config and sql files.",
-)
-
-parser.add(
-    "-rp",
-    "--replace_flag",
-    required=True,
-    help="Flag for replacing existing config (if available).",
-    choices=["Y", "N"],
-)
-
-parser.add(
-    "-c",
-    "--connection_type",
-    required=True,
-    help="Database connection type. Values in POSTGRES, SQLITE",
-)
-
-parser.add("-dh", "--db_host", required=True, help="Database host server.")
-
-parser.add("-dpt", "--db_port", required=True, help="Database host server port.")
-
-parser.add("-dp", "--db_password", required=True, help="Database host server password.")
-
-parser.add("-du", "--db_user", required=True, help="Database User.")
-
-parser.add(
-    "-dt", "--db_target_db", required=True, help="Target Database for sql execution."
-)
-
-
-def init_config(args):
+def init_config():
     """command function"""
     print(format_header())
     print("Starting configuration of client!")
 
-    workdir = args.workdir
-    db_host = args.db_host
-    db_port = args.db_port
-    db_user = args.db_user
-    db_password = args.db_password
-    db_target_db = args.db_target_db
-
-    connection_type = args.connection_type
-    replace_flag = args.replace_flag
+    workdir = input("Input working directory (where to store the db config) : \n")
+    connection_type = input("Input Connection TYPE ([POSTGRES/SQLITE]) : \n")
+    replace_flag = input("Replace Existing Config (if any) ? : \n")
+    db_host = input("Input Database Host (if necessary) : \n")
+    db_port = input("Input Database Port (if necessary) : \n")
+    db_user = input("Input Database User (if necessary) : \n")
+    db_password = getpass("Input Database Password (if necessary) : \n")
+    db_target_db = input("Input Target Database (if sqlite, path to db) : \n")
 
     config_data = {
         "DB_HOST": db_host,
@@ -84,8 +47,7 @@ def init_config(args):
 
 def main():
     """Function that will act just as a conveyer and will be mapped to the poetry commands."""
-    args = parser.parse_args()
-    init_config(args)
+    init_config()
 
 
 if __name__ == "__main__":
---
+++ data_bearer/connectors/sqlitesql.py
@@ -0,0 +1,73 @@
+"""
+Connector for SQLITE database interaction.
+"""
+import logging
+import sqlite3
+import pandas as pd
+from data_bearer.connectors.generic_connector import Connector
+
+
+class SqliteConnector(Connector):
+    """Class for Sqlite connectivity."""
+
+    __package_name__ = "sqlite"
+
+    def __init__(
+        self,
+        db_target_database=None,
+        log_level="WARN",
+    ):
+        """Connect to Sqlite using connection properties."""
+
+        self.set_logger(log_level)
+
+        if db_target_database:
+            logging.info("All properties available, creating connection !")
+            try:
+                self.connection = sqlite3.connect(db_target_database)
+            except:
+                self.connection = None
+                logging.error(
+                    "Unable to initialize connection, please check db credentials"
+                )
+        else:
+            logging.warning(
+                """Missing connection properties,
+                please make sure to correctly specify connection params."""
+            )
+            self.connection = None
+
+    def __str__(self) -> str:
+        return f"connection for : {self.__package_name__}"
+
+    def __repr__(self) -> str:
+        return f"connection for : {self.__package_name__}"
+
+    def fetch_data(self, sql_query) -> pd.DataFrame:
+        """Given class connection, return pandas Dataframe with db content."""
+        if self.connection:
+            logging.info("Connection available, starting data read!")
+            logging.info("Opening new db cursor.")
+            try:
+                cursor = self.connection.cursor()
+                local_data = cursor.execute(sql_query).fetchall()
+                column_names = [i[0] for i in cursor.description]
+                local_df = pd.DataFrame(local_data, columns=column_names)
+            except:
+                logging.error(
+                    f"Something went wrong in the sql execution. Sql code : {sql_query}"
+                )
+                local_df = pd.DataFrame()
+            logging.info("Query execution complete, closing cursor.")
+            cursor.close()
+        else:
+            logging.warning("Connection has to be properly initialized first.")
+            local_df = pd.DataFrame()
+
+        return local_df
+
+
+if __name__ == "__main__":
+    con = SqliteConnector()
+    test_df = con.fetch_data("SELECT CURRENT_DATE;")
+    print(test_df.info())
